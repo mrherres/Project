4bc061039088d9b7084c32b305e7c3d244122d10
mrherres <t.r.heeres@student.rug.nl>
2021-06-11 01:18:39 +0200

    Beginning of actually moving the pawns, we can now spawn a blue pawn if player 1 rolls a 6

--- move.php
+++ move.php
@@ -1,1 +1,22 @@
 <?php
+#$newdata = ['rolled' => $_POST['rolled'], 'name' => $_POST['name']];
+$checkFile = file_get_contents("data/players.json");
+$check = json_decode($checkFile, true);
+if($check["player1"]["name"] === $_POST["name"]){
+    $checkFile2 = file_get_contents("data/gamestate.json");
+    $check2 = json_decode($checkFile2, true);
+    $check2["field"]["p31"] = "blue";
+    $json_object = json_encode($check2);
+    file_put_contents('data/gamestate.json', $json_object);
+    }
+else{
+    $checkFile2 = file_get_contents("data/gamestate.json");
+    $check2 = json_decode($checkFile2, true);
+    $check2["field"]["p1"] = "green";
+    $json_object = json_encode($check2);
+    file_put_contents('data/gamestate.json', $json_object);
+}
+
+header("Content-Type: application/json");
+
+
--- scripts/game.js
+++ scripts/game.js
@@ -7,8 +7,42 @@ function movePawn(id) {
 $(function() {
 
     $('#dice').click(function () {
+        const abba = window.location.search;
+        const urlParams = new URLSearchParams(abba);
+        const pname = urlParams.get('name')
         var dice_result = Math.floor(Math.random() * 6)+1;
         $('#dice').attr('src', "img/dice" + dice_result + ".png")
-        $("#dice-text").html("You rolled:");
+        $("#dice-text").html("You rolled: " + dice_result);
+        if(dice_result === 6){
+            // Hier moet iets komen van, wil je een nieuwe pion opzetten
+            let request = $.ajax({
+                url: "http://localhost/Project/move.php",
+                method: "POST",
+                data: {
+                    "rolled" : dice_result,
+                    "name" : pname
+                },
+                dataType: "json"
+            });
+            //request.done(function(data){
+                //console.log(data);
+            //});
+        }
     });
 });
+
+(function worker2() {
+    $.ajax({
+        url: 'data/gamestate.json',
+        success: function(data) {
+            if(data["field"].p31 === "blue"){
+                $("#p31").html("").addClass("round_blue pawn");
+            }
+
+        },
+        complete: function() {
+            // Schedule the next request when the current one's complete
+            setTimeout(worker2, 1000);
+        }
+    });
+})();
